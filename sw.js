ba9c2d36a5c1f2b42749f815fbdd00011aaf2c25
adilopus <46967190+adilopus@users.noreply.github.com>
2026-06-05 12:01:08 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
-javascript
-const CACHE_NAME = 'vechnost-v3';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'vechnost-v2';
+const urlsToCache = ['/', '/index.html'];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
     self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
